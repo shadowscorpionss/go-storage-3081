b7ed0351a3184cdca9f187fa7def913746ae2f03
shadowscorpionss <shadowscorpionss@outlook.com>
2023-12-11 23:11:41 +0300

    Merge branch 'feature/db-create-scripts'

---
+++ dbCreate.sql
@@ -0,0 +1,15 @@
+-- Database: db30_8_1
+
+-- DROP DATABASE IF EXISTS db30_8_1;
+
+CREATE DATABASE db30_8_1
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    ICU_LOCALE = 'en-US'
+    LOCALE_PROVIDER = 'icu'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
